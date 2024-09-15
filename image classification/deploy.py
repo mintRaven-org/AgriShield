46a56a2a0acbfc086f9d866554ef349153666c7b
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-09-15 10:32:29 +0530

    [ML]: deployment of keras model using fastAPI

---
+++ image classification/deploy.py
@@ -0,0 +1,38 @@
+import uvicorn
+from fastapi import FastAPI
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+import numpy as np
+
+model = load_model("crop_disease.h5")
+data_cat = ["Wheat black rust", "Wheat powdery mildew"]
+
+img_height = 180
+img_width = 180
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"health": "ok", "status": "working"}
+
+
+@app.post("/predict")
+def get_name(image: str):
+    image_load = tf.keras.utils.load_img(image, target_size=(img_height, img_width))
+    img_arr = tf.keras.utils.array_to_img(image_load)
+    img_bat = tf.expand_dims(img_arr, 0)
+
+    predict = model.predict(img_bat)
+    score = tf.nn.softmax(predict)
+
+    return {
+        "disease": f"{data_cat[np.argmax(score)]}",
+        "confidence": np.max(score) * 100,
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
