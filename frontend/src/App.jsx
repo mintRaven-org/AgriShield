8a149159406aff988dc9a44845327a053cba0a51
Hritesh_Saha <hriteshsaha4@gmail.com>
2024-09-17 10:36:18 +0530

    [Frontend]-adding frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import HomePage from './pages/Home/HomePage';
+import AiAssist from './pages/AI assist/AiAssist';
+import Login from './pages/Login/Login';
+import Signup from './pages/Login/Signup';
+import Profile from './pages/profile';
+
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<Signup/>}/>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/ai-assist" element={<AiAssist />} />
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
